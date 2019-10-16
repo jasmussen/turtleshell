1257355dde267d4949bf00c70865e60665de5a87
jasmussen <joen@automattic.com>
2019-10-16 20:55:41 +0200

    Mountain polish.

--- src/App.js
+++ src/App.js
@@ -226,7 +226,7 @@ function Mountain( { className, currentHeuristic } ) {
 				</svg>
 				<svg className="m__group-fg" style={{ fill: getLightColor( currentHeuristic ) }} width="100" height="200" viewBox="0 0 100 200">
 					<path opacity=".3" d="M50 145h10v-15H50v15zm15-45v15h10v-15H65zm-35 30h10v-10H30v10z" />
-					<path opacity=".1" d="M40 155h10l10-10H50l-10 10zm10-15l-10-10H30l10 10h10zm-10-20l-10-10H20l10 10h10zm25-5l-15 15h10l15-15H65z" />
+					<path fill="black" opacity=".1" d="M40 155h10l10-10H50l-10 10zm10-15l-10-10H30l10 10h10zm-10-20l-10-10H20l10 10h10zm25-5l-15 15h10l15-15H65z" />
 					<path opacity=".6" d="M65 50L55 40H45l10 10h10zm10 25H55l10 10h20L75 75z" />
 					<path opacity=".4" d="M45 50L25 70h10l20-20z" />
 					<path opacity=".2" d="M25 85h10V70H25v15zm30-35v25h10V50H55z" />
